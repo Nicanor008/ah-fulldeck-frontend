71431b4df66ac6a5821c137e4c9671bd4f764d3f
eduhmik <edwinkimaita78@gmail.com>
2019-02-07 09:22:33 +0300

    chore(SASS setup): sets up SASS processing on the client
    - install node sass
    - change the .css files to .scss
    - style the homepage
    
    [Delivers #162948951]

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import LandingPage from "./views/LandingPage";
+import logo from './assets/images/logo.png';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
+      <div className="App">
       <BrowserRouter>
         <LandingPage />
       </BrowserRouter>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+          <h1>For the social creative at heart</h1>
+        </header>
+      </div>
     );
   }
 }
